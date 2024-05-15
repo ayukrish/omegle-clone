0d1b2f8b730c9b690a92621507a5cf141d5b1dce
Ayush Krishnatray - a0k09v3 <Ayush.Krishnatray@walmart.com>
2024-05-15 18:01:53 +0530

    feat: setting up mediastream

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from './LandingPage';
-import Room from './Room';
+import LandingPage from './component/LandingPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<LandingPage />} />
-        <Route path="/room" element={<Room />} />
+        {/* <Route path="/room" element={<Room />} /> */}
       </Routes>
     </BrowserRouter>
-
   );
 }
 
